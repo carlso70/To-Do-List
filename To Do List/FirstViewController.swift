ed3139aa949d7ecd5349d691bd0dc449272010e3
Jimmy Carlson <carlso70@purdue.edu>
2015-10-23 15:51:34 -0400

    A simple toDoList app that tests saving data and deleting data throught multipe view controllers

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoList = [String]() //other controllers can reference this
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var toDoListTable: UITableView!
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return toDoList.count
+
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        //this is the code to swipe left and delete an item from the table, but this is also the code to edit the item
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            toDoList.removeAtIndex(indexPath.row)
+            
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            
+            toDoListTable.reloadData()
+        }
+        
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        toDoListTable.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil) { //if this standard user default exists then run through the method, does check because if there wasnt it would be loading nil array
+        toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+        
+        }
+    
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoList.append(itemTextField.text!)
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        
+        itemTextField.text = ""
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        itemTextField.resignFirstResponder()
+        return true
+    }
+    
 }
 
